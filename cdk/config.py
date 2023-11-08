802e0a91ebc1755a0805a501263f49c6fdf4cf82
Abdelali12-codes <jadelmoulaa3@gmail.com>
2023-11-08 12:56:29 +0000

    update code

--- cdk/config.py
+++ cdk/config.py
@@ -14,5 +14,5 @@ ACCOUNT = '080266302756'
 route53_conf = {
     "hostedzone_id": "Z05045244G4M5OFGHB4C",
     "record_name": "reactapp.abdelalitraining.com",
-    "certificate_arn": "arn:aws:acm:us-west-2:080266302756:certificate/7048596d-aa16-49f5-8399-2b2e53a4fc5f"
+    "certificate_arn": "arn:aws:acm:us-east-1:080266302756:certificate/326d29bd-95af-4139-ace0-eccb94dbbcfe"
 }
